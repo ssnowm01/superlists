eef8f77fbd2e922248576c3946572af2c08c9b76
Shannon S <ssnowm01@baker.edu>
2017-01-30 22:42:01 -0500

    new URL + view for adding to existing lists. FT passes

--- lists/tests.py
+++ lists/tests.py
@@ -64,18 +64,13 @@ class ListViewTest(TestCase):
         response = self.client.get('/lists/%d/' % (correct_list.id,))
         self.assertEqual(response.contect['list'], correct_list)
 
-    def test_displays_all_items(self):
-        list_ = List.objects.create()
-        Item.objects.create(text='itemey 1', list=list_)
-        Item.objects.create(text='itemey 2', list=list_)
-
     def test_displays_only_items_for_that_list(self):
         correct_list = List.objects.create()
         Item.objects.create(text='itemey 1', list=correct_list)
         Item.objects.create(text='itemey 2', list=correct_list)
         other_list = List.objects.create()
-        Item.objects.create(text='other list item '1, list=other_list)
-        Item.objects.create(text='other list item '2, list=other_list)
+        Item.objects.create(text='other list item 1', list=other_list)
+        Item.objects.create(text='other list item 2', list=other_list)
 
         response = self.client.get('/lists/%d/' % (correct_list.id,))
 
@@ -83,6 +78,12 @@ class ListViewTest(TestCase):
         self.assertContains(response, 'itemey 2')
         self.assertNotContains(response, 'other list item 1')
         self.assertNotContains(response, 'other list item 2')
+
+
+    def test_displays_all_items(self):
+        list_ = List.objects.create()
+        Item.objects.create(text='itemey 1', list=list_)
+        Item.objects.create(text='itemey 2', list=list_)
 
 class ListAndItemModelsTest(TestCase):
 
